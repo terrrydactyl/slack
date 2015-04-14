71dbb73bbb3447bd97c16ac9954bd27de71a07dd
Teresa <tcho708@gmail.com>
2015-04-14 12:16:11 -0700

    Added page to input and display tags

--- slack.py
+++ slack.py
@@ -1,5 +1,8 @@
-import urllib2
+from flask import Flask
+from flask import request
+from flask import render_template
 from HTMLParser import HTMLParser
+import urllib2
 
 
 class MyHTMLParser(HTMLParser):
@@ -11,8 +14,24 @@ class MyHTMLParser(HTMLParser):
     def handle_endtag(self, tag):
         return self.container.append("</%s>" % tag)
 
-response = urllib2.urlopen("http://google.com")
-page_source = response.read()
-parser = MyHTMLParser()
-parser.feed(page_source)
-print parser.container
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    response = urllib2.urlopen(text)
+    page_source = response.read()
+    parser = MyHTMLParser()
+    parser.feed(page_source)
+    return render_template('tags.html', tags=parser.container)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
