eff4d7f64f6b1384dfe689744eeeda4eab97ed27
Teresa <tcho708@gmail.com>
2015-04-15 23:28:01 -0700

    Wrapped tags in <span>.
    Deleted BeautifulSoup and reformatted with HTMLParser instead.

--- slack.py
+++ slack.py
@@ -1,20 +1,39 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import Markup
 from HTMLParser import HTMLParser
 from collections import Counter
-import urllib2
-from bs4 import BeautifulSoup
+import requests
 
 
 class MyHTMLParser(HTMLParser):
     container = []
+    div_wrap = Markup("")
 
     def handle_starttag(self, tag, attrs):
-        return self.container.append("<%s>" % tag)
+        self.container.append("<%s>" % tag)
+        # print self.div_wrap
+        attrs_string = ''
+        print attrs
+        for attr in attrs:
+            str = '%s="%s" ' % (attr[0], attr[1])
+            attrs_string = attrs_string + str
+        if attrs_string and attrs_string[-1] == " ":
+            attrs_string = attrs_string[:-1]
+        print attrs_string
+        self.div_wrap = self.div_wrap + Markup('<span id="%s">' % tag) + \
+                        Markup.escape('<%s %s>' % (tag, attrs_string)) + Markup('</span>')
+
 
     def handle_endtag(self, tag):
-        return self.container.append("</%s>" % tag)
+        self.container.append("</%s>" % tag)
+        # print self.div_wrap
+        self.div_wrap = self.div_wrap + Markup('<span id="%s">' % tag) + \
+                        Markup.escape('</%s>' % (tag)) + Markup('</span>')
+
+    def handle_data(self, data):
+        self.div_wrap = self.div_wrap + Markup.escape(data)
 
 
 app = Flask(__name__)
@@ -27,15 +46,13 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    response = urllib2.urlopen(text)
-    page_source = response.read()
-    soup = BeautifulSoup(page_source)
-    pretty_html = soup.prettify()
+    url = request.form['text']
+    r = requests.get(url)
+    page_source = r.text
     parser = MyHTMLParser()
     parser.feed(page_source)
-    c = Counter(parser.container)
-    return render_template('tags.html', tags=c, content=pretty_html)
+    tag_counter = Counter(parser.container)
+    return render_template('tags.html', tags=tag_counter, content=parser.div_wrap)
 
 
 if __name__ == '__main__':
