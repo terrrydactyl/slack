1a8f07942d214eafe48e10dcfe5143a7da95ec24
Teresa <tcho708@gmail.com>
2015-04-20 13:58:31 -0700

    Edited to only process opening tags.

--- slack.py
+++ slack.py
@@ -18,18 +18,17 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         self.container.append("<%s>" % tag)
         attrs_string = ''
-        for attr in attrs:
-            str = '%s="%s" ' % (attr[0], attr[1])
-            attrs_string = attrs_string + str
-        if attrs_string and attrs_string[-1] == " ":
-            attrs_string = attrs_string[:-1]
+        if attrs:
+            for attr in attrs:
+                str = '%s="%s" ' % (attr[0], attr[1])
+                attrs_string = attrs_string + str
+            if attrs_string and attrs_string[-1] == " ":
+                attrs_string = attrs_string[:-1]
+            tag_string =  Markup.escape('<%s %s>' % (tag, attrs_string))
+        else:
+            tag_string = Markup.escape('<%s>' % tag)
         self.div_wrap = self.div_wrap + Markup('<span class="slack-%s">' % tag) + \
-                        Markup.escape('<%s %s>' % (tag, attrs_string)) + Markup('</span>')
-
-    def handle_endtag(self, tag):
-        self.container.append("</%s>" % tag)
-        self.div_wrap = self.div_wrap + Markup('<span class="slack-end-%s">' % tag) + \
-                        Markup.escape('</%s>' % (tag)) + Markup('</span>')
+                        tag_string + Markup('</span>')
 
     def handle_data(self, data):
         self.div_wrap = self.div_wrap + Markup.escape(data)
@@ -49,7 +48,6 @@ def my_form_post():
     page_source = r.text
     parser = MyHTMLParser()
     parser.feed(page_source)
-    print parser.container.count('</a>')
     tag_counter = Counter(parser.container)
     return render_template('tags.html', tags=sorted(tag_counter.items(), key=itemgetter(0)),
                        content=parser.div_wrap)
